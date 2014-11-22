676c221867e44c3a68b3f3a289e016d102e1c783
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-22 10:30:48 +0000

    CreeperAPI now subclass of APIEntityLiving

--- mw/director/specials/CreeperAPI.java
+++ mw/director/specials/CreeperAPI.java
@@ -2,11 +2,11 @@ package mw.director.specials;
 
 import net.minecraft.entity.Entity;
 import mw.director.API;
-import mw.director.APIEntity;
+import mw.director.APIEntityLiving;
 import mw.director.DirectorPacketHandler;
 import mw.director.EntityDirector;
 
-public class CreeperAPI extends APIEntity {
+public class CreeperAPI extends APIEntityLiving {
 	
 	public CreeperAPI(API api, EntityDirector e, String name, Class<? extends Entity> ec) {
 		super(api, e, name, ec);
